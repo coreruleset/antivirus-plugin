71dcf4b0180aa3b1436d7d4fd6f3c41dfc9e4b72
azurit <jozef@sudolsky.sk>
2021-10-18 20:14:47 +0200

    Add filename into logs, few tweaks

--- plugins/antivirus.lua
+++ plugins/antivirus.lua
@@ -11,35 +11,36 @@ end
 
 function main(filename_or_data)
 	pcall(require, "m")
-	local chunk_size = tonumber(m.getvar("tx.antivirus-plugin_clamav_chunk_size_bytes", "none"))
 	local connect_type = m.getvar("tx.antivirus-plugin_clamav_connect_type")
 	if connect_type == "socket" then
 		module_name = "socket.unix"
 	elseif connect_type == "tcp" then
 		module_name = "socket"
 	else
-		m.log(2, string.format("ClamAV: invalid value '%s' for 'tx.antivirus-plugin_clamav_connect_type' in antivirus-config-before.conf.", connect_type))
+		m.log(2, string.format("Antivirus Plugin ERROR: Invalid value '%s' for 'tx.antivirus-plugin_clamav_connect_type' in antivirus-config-before.conf.", connect_type))
 		return nil
 	end
 	local ok, socket = pcall(require, module_name)
 	if not ok then
-		m.log(2, "ClamAV: lua-socket library not installed, please install it or disable 'tx.antivirus-plugin_enable' in antivirus-config-before.conf.")
+		m.log(2, "Antivirus Plugin ERROR: LuaSocket library not installed, please install it or disable 'tx.antivirus-plugin_enable' in antivirus-config-before.conf.")
 		return nil
 	end
 
 	local file_handle = io.open(filename_or_data, "r")
 	if file_handle ~= nil then
 		data_size = file_handle:seek("end")
+		data_type = "file"
 		file_handle:seek("set", 0)
 	else
 		data_size = string.len(filename_or_data)
+		data_type = "request_body"
 		position_from = 1
 	end
 	-- Empty data, nothing to scan.
 	if data_size == 0 then
 		return nil
 	elseif data_size > tonumber(m.getvar("tx.antivirus-plugin_max_data_size_bytes")) then
-		m.log(2, string.format("ClamAV: Scan aborted, data are too big (see 'tx.antivirus-plugin_max_data_size_bytes' in antivirus-config-before.conf), data size: %s bytes", data_size))
+		m.log(2, string.format("Antivirus Plugin ERROR: Scan aborted, data are too big (see 'tx.antivirus-plugin_max_data_size_bytes' in antivirus-config-before.conf), data size: %s bytes.", data_size))
 		return nil
 	end
 
@@ -57,18 +58,20 @@ function main(filename_or_data)
 		status, error = sck:connect(m.getvar("tx.antivirus-plugin_clamav_address", "none"), tonumber(m.getvar("tx.antivirus-plugin_clamav_port", "none")))
 	end
 	if not status then
-		m.log(2, string.format("ClamAV: Error connecting to antivirus: %s", error))
+		m.log(2, string.format("Antivirus Plugin ERROR: Error connecting to antivirus: %s.", error))
 		return nil
 	end
 
 	sck:send("nINSTREAM\n")
+
+	local chunk_size = tonumber(m.getvar("tx.antivirus-plugin_clamav_chunk_size_bytes", "none"))
 
 	-- Data needs to be streamed in chunks prefixed by binary packed chunk size.
 	-- Streaming is terminated by sending a zero-length chunk.
 	-- Chunk size is configurable but must NOT exceed StreamMaxLength defined in ClamAV configuration file.
 	while true do
 		-- Scanning of uploaded file.
-		if file_handle ~= nil then
+		if data_type == "file" then
 			chunk = file_handle:read(chunk_size)
 		-- Scanning of REQUEST_BODY.
 		else
@@ -109,7 +112,7 @@ function main(filename_or_data)
 		if status == "closed" then
 			break
 		elseif status == "timeout" then
-			m.log(2, "ClamAV: Timeout while scanning file.")
+			m.log(2, "Antivirus Plugin ERROR: Timeout while scanning data.")
 			return nil
 		end
 	end
@@ -121,10 +124,30 @@ function main(filename_or_data)
 	else
 		local virus_name = string.match(output, "^stream: (.+) FOUND$")
 		if virus_name then
-			m.setvar("tx.virus_name", virus_name)
-			return string.format("ClamAV: Virus found in uploaded file: %s", virus_name)
+			m.setvar("tx.antivirus-plugin_virus_name", virus_name)
+			if data_type == "file" then
+				local real_file_name = "<unknown>"
+				local files_tmpnames = m.getvars("FILES_TMPNAMES", "none")
+				for key, tmpfile in pairs(files_tmpnames) do
+					if filename_or_data == tmpfile["value"] then
+						local input_name = string.match(tmpfile["name"], "^FILES_TMPNAMES:(.+)$")
+						local files = m.getvars("FILES", "none")
+						for key2, file in pairs(files) do
+							if file["name"] == string.format("FILES:%s", input_name) then
+								real_file_name = file["value"]
+								break
+							end
+						end
+						break
+					end
+				end
+				m.setvar("tx.antivirus-plugin_file_name", real_file_name)
+				return string.format("Antivirus Plugin: Virus %s found in uploaded file %s.", virus_name, real_file_name)
+			else
+				return string.format("Antivirus Plugin: Virus %s found in request body.", virus_name)
+			end
 		else
-			m.log2(string.format("ClamAV: Unknown response from antivirus: %s", output))
+			m.log(2, string.format("Antivirus Plugin ERROR: Unknown response from antivirus: %s.", output))
 			return nil
 		end
 	end
