a8a27b05c04c26b41d1f947c1c072ccded42910b
azurit <jozef@sudolsky.sk>
2022-05-07 16:06:42 +0200

    Fix compatibility with modsec v3

--- plugins/antivirus.lua
+++ plugins/antivirus.lua
@@ -137,9 +137,10 @@ function main(filename_or_data)
 			m.setvar("tx.antivirus-plugin_virus_name", virus_name)
 			if data_type == "file" then
 				local real_file_name = "<unknown>"
-				local modsec_build = tonumber(m.getvar("MODSEC_BUILD", "none"))
+				--local modsec_build = tonumber(m.getvar("MODSEC_BUILD", "none"))
 				-- Because of the bug in v3 version, this feature is supported only in v2.
-				if modsec_build < 30000000 then
+				--if modsec_build < 30000000 then
+				if _VERSION == "Lua 5.1" then
 					local files_tmpnames = m.getvars("FILES_TMPNAMES", "none")
 					for key, tmpfile in pairs(files_tmpnames) do
 						if filename_or_data == tmpfile["value"] then
